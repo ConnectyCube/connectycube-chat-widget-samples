e7ccca04cd5454532881ca427d5b437873e2e2da
AntonSelezev <anton@connectycube.com>
2025-10-02 13:58:44 +0300

    add replaceable config

---
+++ react-ts/src/config.ts
@@ -0,0 +1,29 @@
+const CONFIG = {
+  credentials: {
+    appId: "REPLACE_APP_ID",
+    authKey: "REPLACE_APP_AUTH_KEY"
+  },
+  widget: {
+    userPresence: {
+      lastSeen: `${REPLACE_ENABLE_USER_LAST_SEEN}` === "true",
+      userStatuses: `${REPLACE_ENABLE_USER_STATUSES}` === "true",
+      chatStatus: `${REPLACE_ENABLE_SHOW_CHAT_STATUS}` === "true",
+    },
+    notification: {
+      show: `${REPLACE_ENABLE_SHOW_NOTIFICATIONS}` === "true",
+      playSound: `${REPLACE_ENABLE_PLAY_SOUND}` === "true"
+    },
+    moderation: {
+      contentReporting: `${REPLACE_ENABLE_CONTENT_REPORTING}` === "true",
+      blockList: `${REPLACE_ENABLE_BLOCK_LIST}` === "true",
+    },
+    calls: {
+      enable: `${REPLACE_ENABLE_CALLS}` === "true",
+    },
+    misc: {
+      previewUrl: REPLACE_ENABLE_URL_PREVIEW === "true",
+    }
+  }
+};
+
+export default CONFIG;
