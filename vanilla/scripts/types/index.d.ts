ee732f4c8f2b4644759f4348a3b30d8b7fe09346
Vlad <vlad@connectycube.com>
2026-01-16 11:29:40 +0200

    Chat Widget v0.39.1 (vanilla)

--- vanilla/scripts/types/index.d.ts
+++
@@ -1,8 +0,0 @@
-import type { SAMAChat, XMPPChat } from './ConnectyCube';
-import ConnectyCube from './ConnectyCube';
-declare const connectycube: ConnectyCube<XMPPChat>;
-export default connectycube;
-export * from './types/enums';
-export type * from './types';
-export type { XMPPChat, SAMAChat };
-//# sourceMappingURL=index.d.ts.map
--- vanilla/scripts/types/videocalling/WebRTCSignalingProcessor.d.ts
+++
@@ -1,8 +0,0 @@
-import type WebRTCClient from './WebRTCClient';
-import type { Calls } from '../types';
-export default class WebRTCSignalingProcessor {
-    delegate: WebRTCClient;
-    constructor(delegate: WebRTCClient);
-    onMessage: (userID: number, extension: Calls.ExtensionParams) => void;
-}
-//# sourceMappingURL=WebRTCSignalingProcessor.d.ts.map
